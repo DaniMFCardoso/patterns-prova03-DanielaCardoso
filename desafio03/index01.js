fa787934084917cbe6bae658c3f733da1f02a063
Daniela Miranda Fernandez Cardoso <daniela.419@alunosatc.edu.br>
2025-11-27 19:30:58 -0300

    add

---
+++ desafio03/index01.js
@@ -0,0 +1,11 @@
+class UsuarioService {
+    criarUsuario(nome) {
+        console.log("Usuário criado:", nome);
+    }
+}
+
+class EmailService {
+    enviarBoasVindas(email) {
+        console.log("E-mail enviado para:", email);
+    }
+}
---
+++ desafio03/index03.js
@@ -0,0 +1,28 @@
+// GRASP / Information Expert
+
+class Item {
+    constructor(nome, preco, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
+
+class Pedido {
+    constructor(itens) {
+        this.itens = itens;
+    }
+
+    calcularTotal() {
+        return this.itens.reduce((total, item) => {
+        return total + item.preco * item.quantidade;
+        }, 0);
+    }
+}
+
+const pedido = new Pedido([
+    new Item("Mouse", 50, 2),
+    new Item("Teclado", 150, 1)
+]);
+
+console.log(pedido.calcularTotal()); 
